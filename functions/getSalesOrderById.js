37374dc6c488cce18d7b2d8d51b76be23aa1bfc2
jdunn89 <jdunn@nchannel.com>
2018-09-28 10:07:34 -0400

    Updating ById stub functions into include paging

--- functions/getSalesOrderById.js
+++ functions/getSalesOrderById.js
@@ -2,16 +2,26 @@
 
 module.exports = async function (flowContext, payload) {
   let orders = [];
-  let out;
-  let invalid = false;
-  for (const remoteID of payload.remoteIDs) {
-    await this.queryOrderInfo(`${this.baseUri}/v2/orders/${remoteID}`).then(async response => {
+  let calls = [];
+
+  payload.remoteIDs.forEach(id => {
+    calls.push(`${this.baseUri}/v2/orders/${id}`);
+  });
+
+  let hasMore = false;
+  const currentPage = payload.page;
+  const currentPageIndex = currentPage - 1;
+  const pageSize = payload.pageSize;
+
+  const queries = calls.slice(currentPageIndex * pageSize, currentPage * pageSize);
+
+  for (const query of queries) {
+    await this.queryOrderInfo(query).then(async response => {
       await this.processOrder(response.body);
       orders.push(response.body);
     }).catch(response => {
-      invalid = true;
       if (response.endpointStatusCode == 404) {
-        out = {
+        return {
           endpointStatusCode: response.endpointStatusCode,
           statusCode: 204,
           payload: []
@@ -22,13 +32,18 @@ module.exports = async function (flowContext, payload) {
     });
   }
 
-  if (!invalid) {
-    return {
-      endpointStatusCode: 200,
-      statusCode: 200,
-      payload: orders
-    };
-  } else {
-    return out;
+  const totalCount = calls.length;
+  const totalPages = Math.ceil(totalCount / pageSize);
+  hasMore = currentPage < totalPages;
+
+  this.info(`Found ${totalCount} total orders.`);
+  if (totalCount > 0) {
+    this.info(`Returning ${orders.length} orders from page ${currentPage} of ${totalPages}.`);
   }
+
+  return {
+    endpointStatusCode: 200,
+    statusCode: hasMore ? 206 : orders.length > 0 ? 200 : 204,
+    payload: orders
+  };
 };
